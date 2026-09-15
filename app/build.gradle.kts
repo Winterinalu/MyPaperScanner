9cddcd20ecd1d6205c53d8b9a534e9fed7100140
Winterinalu <ZnCloud79@gmail.com>
2026-09-16 07:11:24 +0800

    removed r8/pro guard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,12 +40,7 @@ android {
             if (keystorePath != null) {
                 signingConfig = signingConfigs.getByName("release")
             }
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            isMinifyEnabled = false
         }
     }
     compileOptions {
